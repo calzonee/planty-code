ecc2e449f4e9c3f7911c2dbfa52cd0ad17912307
Leon Pablo <pablo.martinez@gmx.de>
2024-02-21 17:52:32 +0100

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import firebaseConfig from "../firebase.json"
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+createApp(App).mount('#app')
