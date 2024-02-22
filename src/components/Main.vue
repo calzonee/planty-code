244f284735e4257141a6788bff0847ace31b1fde
Leon Pablo <pablo.martinez@gmx.de>
2024-02-22 02:11:09 +0100

    Add Impressum and README.md

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <h1>Planty Code</h1>
+        <p>Impressum</p>
+        <p>Unternemen: Planty Code</p>
+        <p>Rechtsform: Einzelunternehmen</p>
+        <p>Naundorfer Str. 20</p>
+        <p>01139 Dresden</p>
+        <br>
+        <p>Tel.: +49 15110759275</p>
+        <p>E-Mail: pablo.oliva@outlook.de</p>
+        <p>Website: plantycode.com</p>
+        <br>
+        <p>Geschäftsleitung:</p>
+        <p>Leon Pablo Oliva Martinez</p>
+    </div>
+</template>
